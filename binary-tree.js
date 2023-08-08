47aa4b490d530f51cc50b8822a85a80ac82d4404
Karmen Too <karmen.tanaka@gmail.com>
2023-08-08 14:58:18 +0900

    done

--- binary-tree.js
+++ binary-tree.js
@@ -16,14 +16,25 @@ class BinaryTree {
   /** minDepth(): return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
 
-  minDepth() {
+  minDepth(node = this.root) {
+    if (!node) return 0
 
+    let left = this.minDepth(node.left)
+    let right = this.minDepth(node.right)
+
+    return 1 + Math.min(left, right)
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
-  maxDepth() {
+  maxDepth(node = this.root) {
+    if (!node) return 0
+
+    let left = this.maxDepth(node.left)
+    let right = this.maxDepth(node.right)
+
+    return 1 + Math.max(left, right)
 
   }
 
@@ -31,13 +42,59 @@ class BinaryTree {
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+    let result = 0
 
+    const helper = (node) => {
+      console.log('node is', node)
+      if (node === null) {
+        return 0
+      }
+      let left = helper(node.left)
+      console.log('left is', left)
+      let right = helper(node.right)
+      console.log('right is', right)
+      //this is the whole tree, do we want the whole tree?
+      result = Math.max(result, left + right + node.val, left + node.val, right + node.val)
+      console.log('result is', result)
+      //or do we want a part of the tree to return? to combine with other nodes
+      return Math.max(0, node.val + left, node.val + right)//we can only traverse
+      //down one path, so we choose none of this, or the left path or the right path
+
+    }
+
+    helper(this.root)
+
+    return result
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
-  nextLarger(lowerBound) {
+  nextLarger(lowerBound, node = this.root, lowest = null) {
+    //have to traverse all of the nodes to see the smallest num that is larger than lowerBound
+    if (!this.root) return null
+    if (!node) return 99
+    let left = this.nextLarger(lowerBound, node.left, lowest)
+    let right = this.nextLarger(lowerBound, node.right, lowest)
+
+    if (node.val > lowerBound) {
+
+      if (!lowest) {
+
+        lowest = node.val
+      } else if (node.val > lowest) {
+        lowest = node.val
+      }
+
+    }
+
+
+    if (!lowest && (left === 99 || left === null) && (right === 99 || right === null)) return null
+    if (lowest && left && !right) return Math.min(lowest, left)
+    if (lowest && right && !left) return Math.min(lowest, right)
+    console.log('last should be here')
+    if (lowest && !left && !right) return lowest
+    return Math.min(lowest, left, right)
 
   }
 
@@ -46,7 +103,36 @@ class BinaryTree {
    * (i.e. are at the same level but have different parents. ) */
 
   areCousins(node1, node2) {
+    //use breadth first 
+    let currentNode = this.root
+    let parents = [this.root]
+    let children = []
 
+    while (parents.length > 0) {
+
+      if (currentNode.left == node1 || currentNode.right == node2 && currentNode.right == node1 || currentNode.right == node2) {
+        //found it but it is the same parent 
+        return false
+      }
+      if ((currentNode.left == node1 || currentNode.left == node2) && (currentNode.right != node1 && currentNode.right != node2) ||
+        (currentNode.right == node1 || currentNode.right == node2) && (currentNode.left != node1 && currentNode.left != node2)) {
+        //only one child has a node, see if the other toTraverse has the other one
+        toTraverse.pop()
+        currentNode = toTraverse[toTraverse.length - 1]
+      }
+      toTraverse.pop()
+      //if this node has a left or right put it in toTraverse
+      if (currentNode.left !== null) {
+
+        toTraverse.push(currentNode.left)
+      }
+      if (currentNode.right !== null) {
+
+        toTraverse.push(currentNode.right)
+      }
+      currentNode = toTraverse[toTraverse.length - 1]
+    }
+    return false
   }
 
   /** Further study!
@@ -68,7 +154,7 @@ class BinaryTree {
    * of two nodes in a binary tree. */
 
   lowestCommonAncestor(node1, node2) {
-    
+
   }
 }
 
--- tree.js
+++ tree.js
@@ -15,20 +15,81 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    let final = 0
+    if (this.root) {
+      let currentNode = this.root
+      let toTraverse = [this.root]
+
+      while (toTraverse.length > 0) {
+        //add the current Node's value to final
+        final = final + currentNode.val
+        //remove the current node from arr
+        toTraverse.pop()
+        //if current node has children, add it to the array
+        if (currentNode.children) {
+          currentNode.children.forEach(c => toTraverse.push(c))
+        }
+        //set current node to the first child
+        currentNode = toTraverse[toTraverse.length - 1]
+      }
+    }
+
+    return final
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    let final = 0
+    if (this.root) {
+      let currentNode = this.root
+      let toTraverse = [this.root]
 
+      while (toTraverse.length > 0) {
+        //add the current Node's value to final if the current node's value is even
+        if (currentNode.val % 2 == 0) {
+          final++
+        }
+        //remove the current node from arr
+        toTraverse.pop()
+        //if current node has children, add it to the array
+        if (currentNode.children) {
+          currentNode.children.forEach(c => toTraverse.push(c))
+        }
+        //set current node to the first child
+        currentNode = toTraverse[toTraverse.length - 1]
+      }
+    }
+
+    return final
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    let final = 0
+    if (this.root) {
+      let currentNode = this.root
+      let toTraverse = [this.root]
 
+      while (toTraverse.length > 0) {
+        //add the current Node's value to final if the current node's value is even
+        if (currentNode.val > lowerBound) {
+          final++
+        }
+        //remove the current node from arr
+        toTraverse.pop()
+        //if current node has children, add it to the array
+        if (currentNode.children) {
+          currentNode.children.forEach(c => toTraverse.push(c))
+        }
+        //set current node to the first child
+        currentNode = toTraverse[toTraverse.length - 1]
+      }
+    }
+
+    return final
   }
 }
 
